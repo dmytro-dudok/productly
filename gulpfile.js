77abca89dba4830059cb70ef3e01c6d3a5be60dc
Dmytro <56408843+dmytro-dudok@users.noreply.github.com>
2021-03-31 18:43:41 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const {task, src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
 
 task('scss', () => {
     return src('./scss/**/*.scss')
     .pipe(sass())
-    .pipe(dest('./css'));
+    .pipe(dest('./css'))
+    .pipe(browserSync.reload({stream: true});
 })
 
 task('watch', () => {
