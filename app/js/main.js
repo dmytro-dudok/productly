1fbc18ecba589b095ce8f1d342a08c8b19dddbed
dmytro-dudok <ddudok65@gmail.com>
2021-04-26 17:22:18 +0300

    added all sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-$(function(){
+const slides = document.querySelectorAll(".marketing__slider-item");
+const dots = document.querySelectorAll(".slider-dot");
 
+console.log(slides)
+slides.forEach((slide, index) => {
+  slide.addEventListener("mouseover", (e) => {
+  
+    console.log(`hello ${index}`)
+    slide.classList.toggle('current-slide')
+    dots[index].classList.toggle('current-dot')
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,49 @@
-const {task, src, dest, watch} = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
+const { task, src, dest, watch, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
 
+task("html", () => {
+  return src("./app/*.html").pipe(browserSync.reload({ stream: true }));
+});
 
-task('scss', () => {
-    return src('./scss/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('./css'))
-    .pipe(browserSync.reload({stream: true});
-})
+task("scss", () => {
+  return src("./app/scss/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({
+        overrideBrowserList: ["last 8 versions"],
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./app/css"))
+    .pipe(browserSync.reload({ stream: true }));
+});
 
-task('watch', () => {
-    watch('./scss/**/*.scss', 'scss')
-})
+task("js", () => {
+  return src("./app/js/**/*.js").pipe(browserSync.reload({ stream: true }));
+});
+
+task("slick", () => {
+  return src("node_modules/slick-carousel/slick/slick.min.js")
+    .pipe(dest("./app/js"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+task("browser-sync", () => {
+  browserSync.init({
+    server: {
+      baseDir: "./app/",
+    },
+  });
+});
+
+task("watch", () => {
+  watch("./app/scss/**/*.scss", parallel("scss"));
+  watch("./app/*.html", parallel("html"));
+  watch("./app/js/**/*.js", parallel("js"));
+});
+
+task("default", parallel(["scss", "html", "js", 'slick', "browser-sync", "watch"]));
